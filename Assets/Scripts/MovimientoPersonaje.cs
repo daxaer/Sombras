c7b86f21f7f463f2a14c744ae3ec6aa599e59fbc
daxaer <48969078+daxaer@users.noreply.github.com>
2023-09-03 23:12:47 -0600

    corregido tarjetas tienda

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -7,20 +7,12 @@ using UnityEngine.UI;
 
 public class MovimientoPersonaje : MonoBehaviour
 {
-<<<<<<< Updated upstream:Assets/Scripts/MovimientoPersonaje.cs
-    public float velocidadPlayer { get; private set; }
-    public float vidaMaxima { get; private set; }
-    public float vidaActual { get; private set; }
-
-    public float RangoVida { get { return (float)vidaActual / (float)vidaMaxima; } }
-=======
     public Estadisticas estadisticas;
 
     private Vector3 _objetivoArma;
     [SerializeField] private Camera _camera;
     
     public float RangoVida { get { return (float)estadisticas.vidaActual / (float)estadisticas.vidaMaxima; } }
->>>>>>> Stashed changes:Assets/Scripts/Jugador/MovimientoPersonaje.cs
     
     private Vector2 direccionPlayer;
     
@@ -45,14 +37,7 @@ public class MovimientoPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream:Assets/Scripts/MovimientoPersonaje.cs
-        float direccionX = Input.GetAxisRaw("Horizontal");
-        float direccionY = Input.GetAxisRaw("Vertical");
-        direccion = new Vector2(direccionX, direccionY).normalized;
-
-=======
         _objetivoArma = _camera.ScreenToWorldPoint(Input.mousePosition);
->>>>>>> Stashed changes:Assets/Scripts/Jugador/MovimientoPersonaje.cs
 
 
     }
@@ -82,21 +67,17 @@ public class MovimientoPersonaje : MonoBehaviour
         }
         if (collision.CompareTag("Alma"))
         {
-<<<<<<< Updated upstream:Assets/Scripts/MovimientoPersonaje.cs
-            Alma.CantidadAlmas = Alma.CantidadAlmas + 1;
-=======
-            if (collision.GetComponent<SpawnAlmas>().TipoAlma() == 1)
-            {
-                estadisticas.vidaActual = estadisticas.vidaActual + 10f;
-                estadisticas.vidaActual = Mathf.Clamp(estadisticas.vidaActual, 0, estadisticas.vidaMaxima);
-                barraVida.ValorBarraPorcentual(RangoVida);
-                barraVida.ValorVidaActual(estadisticas.vidaActual);
-            }
-            else
-            {
-                Alma.CantidadAlmas = Alma.CantidadAlmas + 1;
-            }
->>>>>>> Stashed changes:Assets/Scripts/Jugador/MovimientoPersonaje.cs
+            // if (collision.GetComponent<SpawnAlmas>().TipoAlma() == 1)
+            // {
+            //     estadisticas.vidaActual = estadisticas.vidaActual + 10f;
+            //     estadisticas.vidaActual = Mathf.Clamp(estadisticas.vidaActual, 0, estadisticas.vidaMaxima);
+            //     barraVida.ValorBarraPorcentual(RangoVida);
+            //     barraVida.ValorVidaActual(estadisticas.vidaActual);
+            // }
+            // else
+            // {
+            //     Alma.CantidadAlmas = Alma.CantidadAlmas + 1;
+            // }
         }
     }
 
