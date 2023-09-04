d9f3da68d0bf14002b2abfd2b6ad4e5b0c325f7e
GalenKishi <51836206+GalenKishi@users.noreply.github.com>
2023-09-04 01:27:34 -0600

    AdvanceUI_2.0

--- Assets/Scripts/ScriptableObjects/TitleSubtile.cs
+++ Assets/Scripts/ScriptableObjects/TitleSubtile.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [CreateAssetMenu(fileName = "Header", menuName = "TitleAndSubtitle")]
+
 public class TitleSubtile : ScriptableObject
 {
     [SerializeField]private string title;
     [SerializeField]private string subtile;
+    [SerializeField]private Image backgroundImage;
+
 
     public string Title { get { return title; } }
 
     public string Subtile { get {  return subtile; } }
+
+    public Image BackgroundImage { get {  return backgroundImage; } }
 }
