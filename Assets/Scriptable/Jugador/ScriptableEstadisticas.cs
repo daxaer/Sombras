1eeb4e09bccd67109fcc11edac603e1bf41a7499
daxaer <48969078+daxaer@users.noreply.github.com>
2023-09-24 15:39:34 -0600

    cambios

--- Assets/Scriptable/Jugador/ScriptableEstadisticas.cs
+++ Assets/Scriptable/Jugador/ScriptableEstadisticas.cs
@@ -15,11 +15,22 @@ public class ScriptableEstadisticas : ScriptableObject
     [SerializeField] private float porcentajeRoboDeVida;
     [SerializeField] private float rangoGolpe;
     [SerializeField] private float velocidadBala;
+    [SerializeField] private bool ataqueMele;
     [Header("Lamparas")] 
     [SerializeField] private float rangoIluminacion;
     [SerializeField] private float tiempoIluminacion;
 
-    public string NombreJugador { get { return nombreJugador[0]; } }
+    public string NombreJugador(int idioma)
+    {
+        if (idioma >= 0 && idioma < nombreJugador.Length)
+        {
+            return nombreJugador[idioma];
+        }
+        else
+        {
+            return "No existe el idioma";
+        }
+    }
     public int VIdaMaxima { get { return vidaMaxima; } }
     public int ATaque { get { return Ataque; } }
     public float VelocidadDeMovimiento { get { return velocidadMovimiento; } }
@@ -27,6 +38,8 @@ public class ScriptableEstadisticas : ScriptableObject
     public float PorcentajeRoboDeVida { get { return porcentajeRoboDeVida; } }
     public float RangoGolpe { get { return rangoGolpe; } }
     public float VelocidadBala { get { return velocidadBala; } }
+
+    public bool AtaqueMele { get { return ataqueMele; } }
     public float RangoIluminacion { get { return rangoIluminacion; } }
     public float TiempoIluminacion { get { return tiempoIluminacion; } }
 }
