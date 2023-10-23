eeb52233664b7eb00b8ac168800a957d62d8c208
daxaer <48969078+daxaer@users.noreply.github.com>
2023-10-23 00:23:46 -0600

    Mejoras Listas

--- Assets/Scriptable/Mejoras/TiendaMenu.cs
+++ Assets/Scriptable/Mejoras/TiendaMenu.cs
@@ -15,7 +15,6 @@ public class TiendaMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        precio = int.Parse(textoPrecio.text);
         efecto = GetComponentInChildren<GestionNivel>();
     }
     // Update is called once per frame
@@ -26,10 +25,9 @@ public class TiendaMenu : MonoBehaviour
 
     public void SubiendoMejora()
     {
+        Debug.Log("El indice es" + efecto.indiceActual);;
         GestionTienda gestionTienda = FindObjectOfType<GestionTienda>();
         gestionTienda.SeleccionarMejora(efecto, nivelmejora,mejora);
     }
-   
-
 }
 
