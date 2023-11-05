75f8fa5e39100db4f9d5b0c5e5295f18037bfb65
GalenKishi <51836206+GalenKishi@users.noreply.github.com>
2023-11-04 23:26:10 -0600

    SubtitulosV.2

--- Assets/Scripts/UI/Subtitulos.cs
+++ Assets/Scripts/UI/Subtitulos.cs
@@ -13,20 +13,26 @@ public class Subtitulos : MonoBehaviour
     [Header("Frases")]
     [SerializeField][Tooltip("Arreglo de frases, puedes poner una frase aqui para que se muestre en la section de descripcion")]
     [Multiline]private string[] _arregloDeFrases;
+
+    [SerializeField]
+    [Tooltip("Arreglo de frases, puedes poner una frase aqui para que se muestre en la section de descripcion")]
+    [Multiline] private string[] _arregloDeFrasesEspanol;
+
+
     [Tooltip("Limite del arreglo de descripciones")][SerializeField]private float _limitArray;
     [Space(1)]
     [Header("Contador de frase(No Tocar)")][Tooltip("Este siguiente significa en que espacio del arreglo de frases esta (No tocar)")]
-    [SerializeField]private int _siguiente;
+    [SerializeField] private int _siguiente;
 
-    [Header("TiempoLimite")][Tooltip("Tiempo de limite de cambiar el texto")]
-    [SerializeField]private float _timerLimit;
+    [SerializeField] private float _timerLimit;
+
     [Space(1)]
     [Header("Contador(No tocar)")][Tooltip("Contador")]
     [SerializeField] private float _timer;
 
     void Start()
     {
-        _textMeshProUGUI.text = _arregloDeFrases[0];
+        StartCoroutine(SwitcherDescription());
     }
 
     void Update()
@@ -35,25 +41,45 @@ public class Subtitulos : MonoBehaviour
         if (_timer > _timerLimit)
         {
             _timer = 0f;
-            _siguiente++;
             StartCoroutine(SwitcherDescription());
-        }
-
-        if (_siguiente == _limitArray)
-        {
-            _siguiente = 0;
-            _textMeshProUGUI.text = _arregloDeFrases[_siguiente];
         }
     }
 
     private IEnumerator SwitcherDescription()
     {
         AnimationDesaparecer();
-        yield return new WaitForSeconds(1);
         AnimationAparecer();
-        _textMeshProUGUI.text = _arregloDeFrases[_siguiente];
+        if (GameManager.Instance.ChangeLenguageTarget() == 0)
+        {
+            _textMeshProUGUI.text = _arregloDeFrasesEspanol[_siguiente];
+        }
+        else
+        {
+            _textMeshProUGUI.text = _arregloDeFrases[_siguiente];
+        }
+        yield return new WaitForSeconds(1);
+        if (_siguiente == _arregloDeFrasesEspanol.Length - 1)
+        {
+            _siguiente = 0;
+        }
+        else
+        {
+            _siguiente += 1;
+        }
     }
 
+    public void ActualizarDescription() 
+    {
+        if (GameManager.Instance.ChangeLenguageTarget() == 0)
+        {
+            _textMeshProUGUI.text = _arregloDeFrasesEspanol[_siguiente];
+        }
+        else
+        {
+            _textMeshProUGUI.text = _arregloDeFrases[_siguiente];
+        }
+    }
+    
     private void AnimationAparecer()
     {
         //_animator.SetBool("Aparecer", true);
